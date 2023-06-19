57293de09f176e014d6063e988a52a6509338891
Laisa <laisacristina2606@gmail.com>
2023-06-19 17:09:18 -0300

    fwat: desafio

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ func main() {
 	for i := 1; i <= 100; i++ {
 		if i % 3 == 0 && i % 5 == 0 {
 			fmt.Println("PingPang")
+		}else if i % 2 != 0 {
+			fmt.Println("Ping")
 		}else if i % 5 == 0 {
 			fmt.Println("Pang")
-		}else if i % 3 == 0 {
-			fmt.Println("Ping")
 		}else {
 			fmt.Println(i)
 		}
